d5f93570a2d62312340b89340482f7b905e7d1dd
zcs110 <zhuchangsheng110@hotmail.com>
2015-06-22 02:12:49 +0800

    创建工程和远程代码管理器

---
+++ News/News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by 朱长昇 on 15/6/22.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ News/News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News
+//
+//  Created by 朱长昇 on 15/6/22.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
